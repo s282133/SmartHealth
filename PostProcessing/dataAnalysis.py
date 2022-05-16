54d154de33d0cef15227d10ede68a81892bf670d
DESKTOP-OVNK5KO\Giulia <s292257@studenti.polito.it>
2022-05-16 10:22:30 +0200

    Pressure Message and Glycemia Message

--- PostProcessing/dataAnalysis.py
+++ PostProcessing/dataAnalysis.py
@@ -26,15 +26,12 @@ class dataAnalysisClass():
     
     def notify(self, topic, msg):
 
-        # la gestione del peso e del questionario non è inclusa !
-
         d = json.loads(msg)
         self.bn = d["bn"]
-        self.sensorName = self.bn.split("/")[3]           # "bn": "http://example.org/sensor1/"  -> "sensor1"
-        self.clientID = self.bn.split("/")[3]  
+        self.clientID = self.bn.split("/")[3]  #splittare stringhe dei topic -> "bn": "http://example.org/sensor1/"  -> "sensor1"
         e = d["e"]
         self.measureType = e[0]["n"]
-        unit = e[0]["u"]
+        self.unit = e[0]["u"]
         self.timestamp = e[0]["t"]
         self.value = e[0]["v"]
 
@@ -42,7 +39,7 @@ class dataAnalysisClass():
             print(f"DataAnalysisBlock received HEARTRATE measure of: {self.value} at time {self.timestamp}")
             week = "35"
             self.manageHeartRate(week)
-        elif (self.measureType == "pressure"):
+        elif (self.measureType == "pressureHigh")or(self.measureType == "pressureLow"):
             print(f"DataAnalysisBlock received PRESSURE measure of: {self.value} at time {self.timestamp}")
             week = "1"
             self.managePressure(week)            
@@ -54,7 +51,9 @@ class dataAnalysisClass():
             print("Measure type not recognized")
 
 
-    # per queste funzioni descritte sotto dovremmo anche farci passare la settimana di gravidanza
+
+    # Funzioni di varifica del superamento della soglia e invio di un messaggio automatico a telegram 
+    # TODO: per queste funzioni descritte sotto dovremmo anche farci passare la SETTIMANA DI GRAVIDANZA
 
     def manageHeartRate(self, week):
         
@@ -68,71 +67,52 @@ class dataAnalysisClass():
                 print(f"DataAnalysisBlock: heart rate is in range")
             else:
                 print(f"DataAnalysisBlock: heart rate is NOT in range") 
-
-                # varifica superamento soglia e invio di un messaggio automatico a telegram 
                 self.catalog = json.load(open("C:\\Users\\Giulia\\Desktop\\Progetto Iot condiviso\\CatalogueAndSettings\\catalog.json"))
                 self.lista = self.catalog["doctorList"]
-
-                messaggio = f"Attention, patient {self.clientID} {self.measureType} is NOT in range: {self.value}. \n What do you want to do?"
-                patientID = self.clientID 
-                self.telegramID = self.findDoctor(patientID)
+                messaggio = f"Attention, patient {self.clientID} {self.measureType} is NOT in range, the value is: {self.value} {self.unit}. \n What do you want to do?"
+                self.telegramID = self.findDoctor(self.clientID)
                 if self.telegramID > 0:
                     mybot.send_alert(self.telegramID, messaggio, "heartrate on", "heartrate off")
                 else:
                     print("Doctor not found for this patient")
 
-                
     def managePressure(self, week):
-        # TODO: evaluate pressure according to thresholds due to week of pregnancy
         thresholdsPR = self.thresholdsFile["pressure"]
         for rangePR in thresholdsPR:
             weekmin = rangePR["weekrange"].split("-")[0]
             weekmax = rangePR["weekrange"].split("-")[1]
             if (week >= weekmin and week <= weekmax):
-                if (self.value >= rangePR["min"] and self.value <= rangePR["max"]):
+                if (int(self.value) >= int(rangePR["min"]) and int(self.value) <= int(rangePR["max"])):
                     print(f"DataAnalysisBlock: pressure is in range")
-                    # TODO: send message to MQTT broker
                 else:
                     print(f"DataAnalysisBlock: pressure is NOT in range") 
-                    # varifica superamento soglia e invio di un messaggio automatico a telegram 
                     self.catalog = json.load(open("C:\\Users\\Giulia\\Desktop\\Progetto Iot condiviso\\CatalogueAndSettings\\catalog.json"))
                     self.lista = self.catalog["doctorList"]
-
-                    messaggio = f"Attention, patient {self.clientID} {self.measureType} is NOT in range: {self.value}. \n What do you want to do?"
-                    patientID = self.clientID 
-                    self.telegramID = self.findDoctor(patientID)
+                    messaggio = f"Attention, patient {self.clientID} {self.measureType} is NOT in range, the value is: {self.value} {self.unit}. \n What do you want to do?"
+                    self.telegramID = self.findDoctor(self.clientID)
                     if self.telegramID > 0:
                         mybot.send_alert(self.telegramID,messaggio, "pression on", "pression off")
                     else:
                         print("Doctor not found for this patient")
 
-
     def manageGlycemia(self, week):
-        # TODO: evaluate glycemia according to thresholds due to week of pregnancy
         thresholdsGL = self.thresholdsFile["glycemia"]
         for rangeGL in thresholdsGL:
             weekmin = rangeGL["weekrange"].split("-")[0]
             weekmax = rangeGL["weekrange"].split("-")[1]
             if (week >= weekmin and week <= weekmax):
-                #if (self.value >= rangeGL["min"] and self.value <= rangeGL["max"]):
-                if (self.value <= 1):
-
+                if (int(self.value) >= int(rangeGL["min"]) and int(self.value) <= int(rangeGL["max"])):
                     print(f"DataAnalysisBlock: glycemia is in range")
-                    # TODO: send message to MQTT broker
                 else:
                     print(f"DataAnalysisBlock: glycemia is NOT in range") 
-                    # varifica superamento soglia e invio di un messaggio automatico a telegram 
                     self.catalog = json.load(open("C:\\Users\\Giulia\\Desktop\\Progetto Iot condiviso\\CatalogueAndSettings\\catalog.json"))
                     self.lista = self.catalog["doctorList"]
-
-                    messaggio = f"Attention, patient {self.clientID} {self.measureType} is NOT in range: {self.value}. \n What do you want to do?"
-                    patientID = self.clientID 
-                    self.telegramID = self.findDoctor(patientID)
+                    messaggio = f"Attention, patient {self.clientID} {self.measureType} is NOT in range, the value is: {self.value} {self.unit}. \n What do you want to do?" 
+                    self.telegramID = self.findDoctor(self.clientID)
                     if self.telegramID > 0:
                         mybot.send_alert(self.telegramID,messaggio, "glycemia on", "glycemia off")
                     else:
                         print("Doctor not found for this patient")
-
 
     def findDoctor(self, patientID):
         telegramID = 0
@@ -150,8 +130,6 @@ class dataAnalysisClass():
 
 
 
-
-# Classe Bot Telegram
 class SwitchBot:
     def __init__(self, token, broker, port, topic):
         self.tokenBot = token
@@ -170,12 +148,9 @@ class SwitchBot:
                                'callback_query': self.on_callback_query}).run_as_thread()
 
     def send_alert(self,telegramID,messaggio,cmd_on,cmd_off): 
-
         buttons = [[InlineKeyboardButton(text=f'MONITORING 🟡',    callback_data=cmd_on), 
                    InlineKeyboardButton(text=f'NOT MONITORING ⚪', callback_data=cmd_off)]]
-
         keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
-        #self.bot.sendMessage(telegramID, text=f'Attention, patient {clientID} {measureType} is NOT in range: {measure}. \n What do you want to do?', reply_markup=keyboard)
         self.bot.sendMessage(telegramID, text=messaggio, reply_markup=keyboard)
 
     def on_chat_message(self, msg):
@@ -194,15 +169,6 @@ class SwitchBot:
 
         if message == "/monitora_paziente": 
             self.bot.sendMessage(chat_ID, text='Start monitoring')
-        
-        # elif message == "/temperature" :
-        #     #r = requests.get('http://192.168.1.254:8080/temperature') #funziona per il cellulare
-        #     r = requests.get('http://127.0.0.1:8080/temperature') 
-        #     j_temp = r.json() 
-        #     vett = j_temp['e']
-        #     primo = vett[0]
-        #     temp = float(primo['v'])
-        #     self.bot.sendMessage(chat_ID, text='La temperatura è: '+ str(temp))
 
     def on_callback_query(self,msg):
         query_ID , chat_ID , query_data = telepot.glance(msg,flavor='callback_query')
@@ -215,7 +181,6 @@ class SwitchBot:
 
 
 if __name__ == "__main__":
-
     
     # SwitchBot per connettersi al Bot telegram
     conf=json.load(open("C:\\Users\\Giulia\\Desktop\\Progetto Iot condiviso\\CatalogueAndSettings\\settingsTelegram.json"))
@@ -225,17 +190,16 @@ if __name__ == "__main__":
     topic = conf["mqttTopic"]
     mybot=SwitchBot(token,broker,port,topic)
 
-
+    # dataAnalysis per analizzare le soglie e mandare il messaggio telegram
     conf = json.load(open("C:\\Users\\Giulia\\Desktop\\Progetto Iot condiviso\\CatalogueAndSettings\\settings.json"))
     broker = conf["broker"]
     port = conf["port"]
     MQTTpubsub = dataAnalysisClass("rpiSub", "P4IoT/SmartHealth/#", broker, port)
-    
     MQTTpubsub.start()    
 
     while True:
         time.sleep(1)
 
-    MQTTsubscriber.stop()
+    #MQTTsubscriber.stop()
 
 
