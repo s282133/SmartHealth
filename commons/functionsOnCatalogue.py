038e60241b8f83183f13fea6732ce632ba13b0a2
s282133 <s282133@studenti.polito.it>
2022-05-26 19:57:04 +0200

    weeks logic inserted. Still to be tested!

---
+++ commons/functionsOnCatalogue.py
@@ -0,0 +1,27 @@
+import json
+import sys, os
+import json
+sys.path.insert(0, os.path.abspath('..'))
+
+filename = sys.path[0] + '\\CatalogueAndSettings\\catalog.json'
+
+dictionaryCatalog=json.load(open(filename,'r'))
+
+
+def retrievePregnancyDayOne(patient_ID):
+    docList=dictionaryCatalog["doctorList"]
+    for _doctors in docList:
+        patientList=_doctors["patientList"] 
+        for _patients in patientList: 
+            if patient_ID == _patients["patientID"]:
+                data= _patients["personalData"]
+                return data["pregnancyDayOne"]
+
+def retrieveOnlineSince(patient_ID):
+    docList=dictionaryCatalog["doctorList"]
+    for _doctors in docList:
+        patientList=_doctors["patientList"] 
+        for _patients in patientList: 
+            if patient_ID == _patients["patientID"]:
+                device=_patients["connectedDevice"]
+                return device["onlineSince"]
