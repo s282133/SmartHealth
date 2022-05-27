6553a6aab31a8d05e08510c0e81a3cac0e0302fa
LAPTOP-6H8AMLRC\ASUS <lau.cube98@yahoo.com>
2022-05-27 11:50:20 +0200

    errore di telepot risolto

--- PostProcessing/dataAnalysis.py
+++ PostProcessing/dataAnalysis.py
@@ -13,7 +13,7 @@ import sys, os
 from pprint import pprint
 
 sys.path.insert(0, os.path.abspath('..'))
-from commons.functionsOnCatalogue import *
+
 
 class dataAnalysisClass():
 
@@ -46,27 +46,6 @@ class dataAnalysisClass():
             self.unit = self.e[0]["u"]
             self.timestamp = self.e[0]["t"]
             self.value = self.e[0]["v"]
-            currY = self.timestamp.split("-")[0]
-            currM = self.timestamp.split("-")[1]
-            currD = self.timestamp.split("-")[2]
-            currDays = int(currY)*365 + int(currM)*30 + int(currD)
-
-            print(f"DataAnalysisBlock: clientID : {self.clientID}")
-            dayOne = retrievePregnancyDayOne(int(self.clientID))            # @todo DA TESTARE
-            dayoneY = dayOne.split("-")[0]
-            dayoneM = dayOne.split("-")[1]
-            dayoneD = dayOne.split("-")[2]
-            dayoneDays = (int(dayoneY) * 365) + (int(dayoneM) * 30) + int(dayoneD)
-
-            week = (currDays - dayoneDays) / 7
-            if(week == 0): 
-                week = 1;
-
-            print(f"TEST: week is {week}")
-
-            #print(f"DataAnalysisBlock: patient dayOne is {dayOne}")
-            #week = self.timestamp
-            print(f"DataAnalysisBlock: timestamp is {self.timestamp}")
 
         if (self.measureType == "heartrate"):
             print(f"DataAnalysisBlock received HEARTRATE measure of: {self.value} at time {self.timestamp}")
@@ -266,13 +245,7 @@ class SwitchBot:
 # mybot.send_weight(self.telegramID,messaggio)
 
 if __name__ == "__main__":
-
-    # conf = sys.path[0] + '\\CatalogueAndSettings\\catalog.json'
-    # broker = conf["broker"]
-    # broker = broker[0]
-    # port = broker[1]
-
-
+    
     # SwitchBot per connettersi al Bot telegram del dottore
     conf_fn = sys.path[0] + '\\CatalogueAndSettings\\settingsDoctorTelegram.json'
     conf=json.load(open(conf_fn))
