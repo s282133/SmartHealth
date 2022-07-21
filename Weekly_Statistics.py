2829f6395848c2697446c0b4a6e07b0b11ea137d
s282133 <s282133@studenti.polito.it>
2022-07-21 10:58:16 +0200

    now messages are correctly published by WeeklyStats

--- Weekly_Statistics.py
+++ Weekly_Statistics.py
@@ -6,6 +6,7 @@ import decimal
 # import sys
 import re
 import sys, os
+from jinja2 import Template
 sys.path.insert(0, os.path.abspath('..'))
 from commons.MyMQTT import *
 from commons.functionsOnCatalogue import *
@@ -15,7 +16,7 @@ ONE_WEEK_IN_SECONDS = 604800
 class statistics():
 
     # MQTT FUNCTIONS
-    def __init__(self, clientID, mqtt_broker, mqtt_port):
+    def __init__(self, clientID, mqtt_broker, mqtt_port, mqtt_topic):
 
         ###########################################################
         #
@@ -37,7 +38,41 @@ class statistics():
 
         self.client_MQTT = MyMQTT(clientID, mqtt_broker, mqtt_port, self)
         self.clientID = clientID
+        self.pub_topic = str(mqtt_topic).replace("+", clientID)
         self.start()
+        self.initialize()
+        while True:
+            self.counter += 1
+            #if self.counter == ONE_WEEK_IN_SECONDS:           
+            if self.counter == 10:       # DA METTERE A 'ONE_WEEK_IN_SECONDS'
+                statsHR = self.computeStatsHR()
+                message = self.messageCreation("heartrate", statsHR)
+                # print(f"{message}")
+                self.myPublish(self.pub_topic, message)                
+                statsGL = self.computeStatsGL()
+                message = self.messageCreation("glycemia", statsGL)            
+                # print(f"{message}")    
+                self.myPublish(self.pub_topic, message)                               
+                statsPRH = self.computeStatsPRH()
+                message = self.messageCreation("pressure_high", statsPRH)                
+                # print(f"{message}")   
+                self.myPublish(self.pub_topic, message)                                         
+                statsPRL = self.computeStatsPRL()
+                message = self.messageCreation("pressure_low", statsPRL)
+                # print(f"{message}")            
+                self.myPublish(self.pub_topic, message)                                      
+                statsTE = self.computeStatsTE()
+                message = self.messageCreation("temperature", statsTE)
+                # print(f"{message}")     
+                self.myPublish(self.pub_topic, message)                              
+                statsWE = self.computeStatsWE()
+                message = self.messageCreation("weight", statsWE)
+                # print(f"{message}")        
+                self.myPublish(self.pub_topic, message)                           
+                self.counter = 0
+            time.sleep(1)
+
+    def initialize(self):
         self.counter = 0
         with open("settings_weeklyStats.json", "r") as rp:
             settings_dict = json.load(rp)
@@ -56,61 +91,23 @@ class statistics():
             self.we_file = allfiles["weight"]
             self.te_file = allfiles["temperature"]
             self.message_structure = settings_dict["message_structure"]
-        while True:
-            self.counter += 1
-            #if self.counter == ONE_WEEK_IN_SECONDS:
-            #print(f"counter : {self.counter}")            
-            if self.counter == 10:       # DA METTERE A 'ONE_WEEK_IN_SECONDS'
-                statsHR = self.computeStatsHR()
-                message = self.message_structure
-                message["parameter"] = "heartrate"
-                message["statistics"]["min"] = list(statsHR)[0]
-                message["statistics"]["avg"] = list(statsHR)[1]
-                message["statistics"]["max"] = list(statsHR)[2]
-                print(f"{message}")
-                # self.myPublish("sometopicHR", message)
-                statsGL = self.computeStatsGL()
-                message["parameter"] = "glycemia"
-                message["statistics"]["min"] = list(statsGL)[0]
-                message["statistics"]["avg"] = list(statsGL)[1]
-                message["statistics"]["max"] = list(statsGL)[2]
-                print(f"{message}")                
-                # self.myPublish("sometopicGL", message)                
-                statsPRH = self.computeStatsPRH()
-                message["parameter"] = "pressure_high"
-                message["statistics"]["min"] = list(statsPRH)[0]
-                message["statistics"]["avg"] = list(statsPRH)[1]
-                message["statistics"]["max"] = list(statsPRH)[2]
-                print(f"{message}")                
-                # self.myPublish("sometopicPRH", message)                
-                statsPRL = self.computeStatsPRL()
-                message["parameter"] = "pressure_low"
-                message["statistics"]["min"] = list(statsPRL)[0]
-                message["statistics"]["avg"] = list(statsPRL)[1]
-                message["statistics"]["max"] = list(statsPRL)[2]
-                print(f"{message}")                   
-                # self.myPublish("sometopicPRL", message)                
-                statsTE = self.computeStatsTE()
-                message["parameter"] = "temperature"
-                message["statistics"]["min"] = list(statsTE)[0]
-                message["statistics"]["avg"] = list(statsTE)[1]
-                message["statistics"]["max"] = list(statsTE)[2]
-                print(f"{message}")                   
-                # self.myPublish("sometopicTE", message) 
-                statsWE = self.computeStatsWE()
-                message["parameter"] = "weight"
-                message["statistics"]["min"] = list(statsWE)[0]
-                message["statistics"]["avg"] = list(statsWE)[1]
-                message["statistics"]["max"] = list(statsWE)[2]
-                print(f"{message}")                   
-                # self.myPublish("sometopicWE", message)
-                print(f"[HEARTRATE] min/avg/max : {statsHR}")
-                print(f"[PRESSURE_HIGH] min/avg/max : {statsPRH}")
-                print(f"[PRESSURE_LOW] min/avg/max : {statsPRL}")
-                print(f"[PRESSURE_TEMPERATURE] min/avg/max : {statsTE}")
-                print(f"[PRESSURE_WEIGHT] min/avg/max : {statsWE}")
-                self.counter = 0
-            time.sleep(1)
+
+    def messageCreation(self, parameter, stats):
+        message = self.message_structure
+        message["parameter"] = parameter
+        try:
+            message["statistics"]["min"] = list(stats)[0]
+        except:
+            message["statistics"]["min"] = "none"
+        try:
+            message["statistics"]["avg"] = list(stats)[1]
+        except:
+            message["statistics"]["avg"] = "none"
+        try:
+            message["statistics"]["max"] = list(stats)[2]
+        except:
+            message["statistics"]["max"] = "none"   
+        return message         
 
     def start (self):
         self.client_MQTT.start()
@@ -119,7 +116,7 @@ class statistics():
         self.client_MQTT.stop()
 
     def myPublish(self, topic, message):
-        #print(f"{self.clientID} publishing {message} to topic: {topic}")
+        print(f"{self.clientID} publishing {message} to topic: {topic}")
         self.client_MQTT.myPublish(topic, message)
     
     def computeStatsHR(self):
@@ -224,5 +221,7 @@ if __name__ == "__main__":
     mqtt_base_topic = mqtt_service["base_topic"]
     mqtt_api = getApiByName(mqtt_service["APIs"],"send_statistics") 
     mqtt_topic = mqtt_api["topic_statistic"]
-    print(f"TOPIC : {mqtt_topic}")
-    Statistics=statistics("WeeklyStat", mqtt_broker=mqtt_broker, mqtt_port=mqtt_port)
+    print(f"TOPIC prima: {mqtt_topic}")
+    pub_mqtt_topic = str(mqtt_topic).replace("{{base_topic}}", mqtt_base_topic)
+    print(f"TOPIC dopo: {pub_mqtt_topic}")
+    Statistics=statistics("WeeklyStat", mqtt_broker=mqtt_broker, mqtt_port=mqtt_port, mqtt_topic= pub_mqtt_topic)
